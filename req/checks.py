fb89b0b9db5e9efbc1d17387ed1c14954d15eeaa
Elli83 <elliothills42@gmail.com>
2020-08-20 19:21:36 +0100

    Added role checks

--- req/checks.py
+++ req/checks.py
@@ -1,2 +1,20 @@
 from . import mongo, errors
 
+import json
+
+roles = json.load(open("./config/settings.json"))['roles']
+
+async def admin(ctx):
+    for r in ctx.author.roles:
+        if r.id == roles['admin']:
+            return True
+    return False
+
+
+async def moderator(ctx):
+    if await admin(ctx):
+        return True
+    for r in ctx.author.roles:
+        if r.id == roles['moderator']:
+            return True
+    return False
